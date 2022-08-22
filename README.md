47c39e0bcadde46f1c6bc99ba04dea4c45078b7c
Leonardo Melendez <leonardo.melendez@uabc.edu.mx>
2022-08-21 23:39:29 -0700

    Spatial Navigation and updated Tizen config

---
+++ README.md
@@ -0,0 +1,7 @@
+# Tizen App
+
+A Smart TV web application demo built for the Tizen platform.
+
+## Resources
+
+- [JavaScript SpatialNavigation](https://github.com/luke-chang/js-spatial-navigation) by Luke Chang
--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,46 @@
-SpatialNavigation.init();
+function handleKeys(event) {
+  const key = event.keyCode;
 
-SpatialNavigation.add({
-  selector: 'input'
-});
+  switch (key) {
+    case UP_ARROW_BUTTON:
+      console.log(`[key] ${key} - arriba`);
+      //navigate(KEY_UP);
+      break;
+    case DOWN_ARROW_BUTTON:
+      console.log(`[key] ${key} - abajo`);
+      //navigate(KEY_DOWN);
+      break;
+    case RETURN_BUTTON:
+      console.log(`[key] ${key} - atras`);
+      break;
+    case ENTER_BUTTON:
+      console.log(`[key] ${key} - seleccionar`);
+      event.target.click();
+      break;
+    default:
+      console.log(`[key] ${key} - no soportado`);
+  }
+}
 
-SpatialNavigation.makeFocusable();
+function initNavigation() {
+  SpatialNavigation.init();
+  SpatialNavigation.add({
+    selector: 'a, input',
+    enterTo: 'default-element',
+    defaultElement: '.default-element',
+    rememberSource: true
+  });
+  SpatialNavigation.makeFocusable();
+  SpatialNavigation.focus();
 
-SpatialNavigation.focus();
+  document.addEventListener('keydown', handleKeys);
+}
+
+const RETURN_BUTTON = 10009,
+  ENTER_BUTTON = 13,
+  LEFT_ARROW_BUTTON = 37,
+  UP_ARROW_BUTTON = 38,
+  RIGHT_ARROW_BUTTON = 39,
+  DOWN_ARROW_BUTTON = 40;
+
+initNavigation();
