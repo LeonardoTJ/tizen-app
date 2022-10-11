e3c2c3eea798e87d3fa2cb842c2d9e103110c314
Leonardo Melendez <leonardo.melendez@uabc.edu.mx>
2022-10-11 08:40:05 -0700

    Config file updates

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, 'src/js/main.js'),
+    // nav: path.resolve(__dirname, 'src/js/nav.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  mode: 'development',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      title: 'Tizen App',
+      filename: 'index.html',
+      template: 'src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: path.resolve(__dirname, "config.xml"), to: path.resolve(__dirname, 'dist/config.xml') },
+        { from: path.resolve(__dirname, ".witsconfig.json"), to: path.resolve(__dirname, 'dist/.witsconfig.json') },
+        { from: path.resolve(__dirname, "icon.png"), to: path.resolve(__dirname, 'dist/icon.png') },
+      ]
+    })
+  ],
+};
